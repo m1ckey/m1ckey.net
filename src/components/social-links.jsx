471b72ce9a6abd81f703faf8c0066235717eae48
m1ckey <m1ckey@m1ckey.net>
2021-03-21 20:26:01 +0100

    edit email

--- src/components/social-links.jsx
+++ src/components/social-links.jsx
@@ -4,8 +4,8 @@ import styles from './social-links.module.css';
 import { FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
 import { MdEmail } from 'react-icons/md';
 
-const rotCount = 8;
-const ciphertext = 'u9ksm#Hu9ksm#6vm|';
+const rotCount = 18;
+const ciphertext = '!Cu}w-R!Cu}w-@"w(';
 
 class SocialLinks extends React.Component {
   constructor(props) {
@@ -22,7 +22,10 @@ class SocialLinks extends React.Component {
         rotCount: state.rotCount - 1,
         email: rot(state.email),
       }));
-      setTimeout(this.handleDecodeEmail, Math.random() * 200 + 100); // hacker flair
+      setTimeout(
+        this.handleDecodeEmail,
+        Math.pow(rotCount - this.state.rotCount, 2) + 10
+      ); // hacker flair
     }
   };
 
@@ -87,13 +90,10 @@ function rot(text) {
     '!"#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~';
 
   let cleartext = '';
-  let i,
-    j,
-    c,
-    len = map.length;
-  for (i = 0; i < text.length; i++) {
-    c = text.charAt(i);
-    j = map.indexOf(c);
+  let len = map.length;
+  for (let i = 0; i < text.length; i++) {
+    let c = text.charAt(i);
+    let j = map.indexOf(c);
     if (j >= 0) {
       c = map.charAt((j + len - 1) % len);
     }
